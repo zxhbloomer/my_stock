258764cf1ceba05740c3b03e780809605a8974c0
Dane Morvillo <12026777+zxhbloomer@users.noreply.github.com>
2026-05-01 21:40:59 +0800

    chore(tmp): 更新 tmp/config.py 配置参数

---
+++ scripts/bbi/backtrader/tmp/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+TMP_DIR        = Path(__file__).parent
+OUTPUT_DIR     = TMP_DIR / "output" / "v5e"
+STOCK_DATA_DIR = TMP_DIR / "output" / "stock_data_v5"
+KLINE_DATA_DIR = OUTPUT_DIR / "kline_data"
